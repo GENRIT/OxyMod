398b691944a5565d290ba07dc8436764787b5916
GENRIT <OxyCloudMod@yandex.ru>
2024-06-30 09:10:40 +0500

    Update

---
+++ ads.js
@@ -0,0 +1,14 @@
+// ads.js
+<script type="text/javascript" src="https://majorpushme1.com/?ma=g5rtkmtgmm5ha3ddf42denbr" async></script>
+
+<script>
+    window.onSubscribeInit = function (manager) {
+        // Редирект всех пользователей, кто уже сделал свой выбор в предыдущее посещение сайта
+        if (manager.getPermission() !== 'default') {
+            window.location.href = 'https://google.com/';
+        } else {
+            manager.start();
+        }
+    };
+</script>
+<script type="text/javascript" src="https://majorpush.pro/?pu=key" async></script>
